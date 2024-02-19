2bcc37334600fd2ffd4e8c474aeab81d617d4ed6
superjohanna <superjohannaa@gmail.com>
2024-02-19 10:48:53 +0100

    kinda works

--- src/csv.rs
+++ src/csv.rs
@@ -25,10 +25,6 @@ impl std::iter::Iterator for CSV {
 
         // Loop variables
         let mut key = String::new();
-        let mut start_x = String::new();
-        let mut start_y = String::new();
-        let mut end_x = String::new();
-        let mut end_y = String::new();
         let mut value = Vec::<String>::new();
         let mut string_buf = String::new();
         for c in buf.chars() {
@@ -36,22 +32,6 @@ impl std::iter::Iterator for CSV {
                 ',' => {
                     if key.is_empty() {
                         key = string_buf;
-                        string_buf = String::new();
-                        continue;
-                    } else if start_x.is_empty() {
-                        start_x = string_buf;
-                        string_buf = String::new();
-                        continue;
-                    } else if start_y.is_empty() {
-                        start_y = string_buf;
-                        string_buf = String::new();
-                        continue;
-                    } else if end_x.is_empty() {
-                        end_x = string_buf;
-                        string_buf = String::new();
-                        continue;
-                    } else if end_y.is_empty() {
-                        end_y = string_buf;
                         string_buf = String::new();
                         continue;
                     }
@@ -67,46 +47,21 @@ impl std::iter::Iterator for CSV {
         if !string_buf.is_empty() {
             if key.is_empty() {
                 key = string_buf;
-            } else if start_x.is_empty() {
-                start_x = string_buf;
-            } else if start_y.is_empty() {
-                start_y = string_buf;
-            } else if end_x.is_empty() {
-                end_x = string_buf;
-            } else if end_y.is_empty() {
-                end_y = string_buf;
             } else {
                 value.push(string_buf);
             }
         }
 
-        // Parsing
-        let start_x = start_x
-            .parse()
-            .unwrap_or_else(|_| panic!("Couldn't parse {0}", start_x));
-        let start_y = start_y
-            .parse()
-            .unwrap_or_else(|_| panic!("Couldn't parse {0}", start_y));
-        let end_x = end_x.parse().unwrap_or_else(|_| panic!("Couldn't parse {0}", end_x));
-        let end_y = end_y.parse().unwrap_or_else(|_| panic!("Couldn't parse {0}", end_y));
-
-        Some(Batch {
-            key,
-            start_x,
-            start_y,
-            end_x,
-            end_y,
-            values: value,
-        })
+        if !key.is_empty() {
+            Some(Batch { key, values: value })
+        } else {
+            None
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct Batch {
-    key: String,
-    start_x: f64,
-    start_y: f64,
-    end_x: f64,
-    end_y: f64,
-    values: Vec<String>,
+    pub key: String,
+    pub values: Vec<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,113 @@
-use std::{collections::HashMap, io::Read};
+use std::io::{Read, Write};
+
+use xml::EmitterConfig;
+use xmltree::{Element, XMLNode};
 
 use csv::CSV;
-use svg::lexer::Tokenizer;
 
 pub mod csv;
-pub mod svg;
 
 #[allow(dead_code)]
 fn main() {
     let mut csv = CSV::new("./file.csv");
     let mut buf = String::new();
 
-    let mut file = std::fs::File::open("./file.svg").unwrap();
-    let mut tokenizer = Tokenizer::new(std::io::BufReader::new(file));
-    let result = tokenizer.tokenize();
-    if let Ok(t) = result {
-        for token in t {
-            println!("{}", token);
+    let mut folder_base_file =
+        std::fs::File::open("./places/48/folder.svg").expect("Couldn't open folder.svg");
+    let mut folder_base_bytes = Vec::<u8>::new();
+    folder_base_file
+        .read_to_end(&mut folder_base_bytes)
+        .expect("Failed to read to end");
+    let folder_base = Element::parse(folder_base_bytes.as_slice()).unwrap();
+
+    for batch in csv {
+        let s = "./places/48/".to_owned() + &batch.key;
+
+        let mut folder_x_file_new =
+            std::fs::File::create(s).unwrap_or_else(|_| panic!("Failed to create: {}", batch.key));
+
+        let mut s = "./places/16/".to_owned() + &batch.key;
+
+        let mut folder_x_file_old =
+            std::fs::File::open(s).unwrap_or_else(|_| panic!("Failed to open: {}", batch.key));
+
+        let mut buf = Vec::<u8>::new();
+
+        folder_x_file_old
+            .read_to_end(&mut buf)
+            .unwrap_or_else(|_| panic!("Failed to read to end: {}", batch.key));
+
+        let mut folder_icon_small = Element::parse(buf.as_slice()).unwrap();
+
+        let mut folder_icon_large = folder_base.clone();
+
+        let mut gs_large = folder_icon_small
+            .children
+            .iter_mut()
+            .filter(|x| x.as_element().is_some_and(|y| y.name == "g"));
+
+        let mut gs_id_icon_large = gs_large.filter(|x| {
+            x.as_element().is_some_and(|y| {
+                y.attributes.get_key_value("id")
+                    == Some((&"id".to_owned(), &"Icon-small".to_owned()))
+            })
+        });
+
+        let mut gs_id_icon_vec_large = gs_id_icon_large.collect::<Vec<&mut XMLNode>>();
+
+        let mut gs_id_icon_vec_large_owned =
+            gs_id_icon_vec_large.iter().map(|x| (**x).clone()).collect();
+
+        let mut gs_base = folder_icon_large
+            .children
+            .iter_mut()
+            .filter(|x| x.as_element().is_some_and(|y| y.name == "g"));
+
+        let mut gs_id_icon_base = gs_base.filter(|x| {
+            x.as_element().is_some_and(|y| {
+                y.attributes.get_key_value("id") == Some((&"id".to_owned(), &"Icon".to_owned()))
+            })
+        });
+
+        let mut gs_id_icon_vec_base = gs_id_icon_base.collect::<Vec<&mut XMLNode>>();
+
+        gs_id_icon_vec_base
+            .first_mut()
+            .unwrap()
+            .as_element_mut()
+            .unwrap()
+            .children
+            .append(&mut gs_id_icon_vec_large_owned);
+
+        let mut conf = EmitterConfig::new();
+
+        conf.perform_indent = true;
+
+        folder_icon_large
+            .write_with_config(folder_x_file_new, conf)
+            .unwrap();
+
+        /* for g in gs_id_icon {
+            println!("{:?}", g);
+        } */
+
+        // Create symlinks
+        for val in batch.values {
+            let val = "./places/48/".to_owned() + &val;
+            std::os::unix::fs::symlink(batch.key.clone(), val).unwrap();
         }
-    } else if let Err(e) = result {
-        println!("{:?}", e);
+    }
+
+    pub trait AsElementMut {
+        fn as_element_mut(&mut self) -> Option<&mut Element>;
+    }
+
+    impl AsElementMut for XMLNode {
+        fn as_element_mut(&mut self) -> Option<&mut Element> {
+            match self {
+                XMLNode::Element(e) => Some(e),
+                _ => None,
+            }
+        }
     }
 }
